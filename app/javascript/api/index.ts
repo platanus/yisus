a8f14d6bd161febc541e28d508bcd0ff0c7a4b0f
nicolasguridi <nguridi@uc.cl>
2022-08-26 16:31:42 -0400

    feat(api): add csrf token to api requests

--- app/javascript/api/index.ts
+++ app/javascript/api/index.ts
@@ -2,7 +2,13 @@ import axios, { type AxiosRequestTransformer, type AxiosResponseTransformer } fr
 import { camelizeKeys } from 'humps';
 import decamelizeKeys from '../utils/decamelizer';
 
+const CSRFToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content') || false;
+
 const api = axios.create({
+  headers: {
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': CSRFToken,
+  },
   transformRequest: [
     data => decamelizeKeys(data),
     ...(axios.defaults.transformRequest as AxiosRequestTransformer[]),
