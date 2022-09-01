68e1f7e0defb90ced88a979aa873f376b3959e79
nicolasguridi <nguridi@uc.cl>
2022-09-01 17:52:27 -0400

    feat(admin): add button to run fetch harvest time reports job

--- app/admin/time_reports.rb
+++ app/admin/time_reports.rb
@@ -1,6 +1,15 @@
 ActiveAdmin.register TimeReport do
   permit_params :from, :to, :billable_hours, :project_id
 
+  collection_action :fetch, method: :get do
+    FetchHarvestTimeReportsJob.perform_now(Date.current)
+    redirect_to collection_path, notice: "¡Reportes obtenidos exitosamente!"
+  end
+
+  action_item :fetch, only: :index do
+    link_to 'Obtener reportes del mes', fetch_admin_time_reports_path
+  end
+
   index do
     column :from
     column :to
