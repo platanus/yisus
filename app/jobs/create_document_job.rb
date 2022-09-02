51e4d32ab8b9a09bce52c6d10758b75f136a3200
nicolasguridi <nguridi@uc.cl>
2022-09-02 14:51:37 -0400

    refactor(jobs): use real uf value in document creation

--- app/jobs/create_document_job.rb
+++ app/jobs/create_document_job.rb
@@ -1,46 +1,59 @@
 class CreateDocumentJob < ApplicationJob
   queue_as :default
 
-  UF_VALUE = 30000
-
   def perform(time_report_id)
-    time_report = TimeReport.find(time_report_id)
-    document_data = document_data(time_report)
-    response = client.post_document(document_data)
-    raise 'Bsale API error' unless response.success?
+    @time_report = TimeReport.find(time_report_id)
 
-    document = response.body
+    document = post_bsale_document
     Document.create!(
       time_report_id: time_report_id,
-      bsale_id: document[:id],
-      url: document[:urlPdfOriginal]
+      bsale_id: document['id'],
+      url: document['urlPdfOriginal']
     )
   end
 
   private
 
-  def client
-    @client ||= BsaleClient.new
+  def bsale_client
+    @bsale_client ||= BsaleClient.new
   end
 
-  def document_data(time_report)
+  def cmf_client
+    @cmf_client ||= CmfClient.new
+  end
+
+  def get_uf_value
+    response = cmf_client.get_uf(Date.current)
+    raise "CMF API error: #{response.body}" unless response.success?
+
+    response.body['UFs'][0]['Valor'].gsub(/[.,]/, '.' => '', ',' => '.').to_f
+  end
+
+  def document_data
     {
       codeSii: 33,
       officeId: 1,
       emissionDate: Time.zone.now.to_i,
       expirationDate: (Time.zone.now + 30.days).to_i,
       declareSii: 1,
-      clientId: time_report.project.customer.bsale_id,
-      details: document_details(time_report)
+      clientId: @time_report.project.customer.bsale_id,
+      details: document_details
     }.to_json
   end
 
-  def document_details(time_report)
+  def document_details
     [{
-      netUnitValue: time_report.project.unit_price * UF_VALUE,
-      quantity: time_report.billable_hours,
-      comment: I18n.l(time_report.from, format: "%B %Y").capitalize,
+      netUnitValue: @time_report.project.unit_price * get_uf_value,
+      quantity: @time_report.billable_hours,
+      comment: I18n.l(@time_report.from, format: "%B %Y").capitalize,
       taxId: [1]
     }]
+  end
+
+  def post_bsale_document
+    response = bsale_client.post_document(document_data)
+    raise "Bsale API error: #{response.body}" unless response.success?
+
+    response.body
   end
 end
--- spec/jobs/create_document_job_spec.rb
+++ spec/jobs/create_document_job_spec.rb
@@ -4,15 +4,41 @@ RSpec.describe CreateDocumentJob, type: :job do
   describe '#perform_now' do
     let(:time_report) { create(:time_report) }
     let(:time_report_id) { time_report.id }
-    let(:bsale_document) { { id: '1', urlPdfOriginal: 'url' } }
-    let(:post_document) { instance_double(ClientResponse, success?: true, body: bsale_document) }
+    let(:post_document) do
+      instance_double(
+        ClientResponse,
+        success?: true,
+        body: { 'id' => '1', 'urlPdfOriginal' => 'url' }
+      )
+    end
     let(:bsale_client) { instance_double(BsaleClient, post_document: post_document) }
+    let(:cmf_response) do
+      instance_double(
+        ClientResponse,
+        success?: true,
+        body: { 'UFs' => [{ 'Valor' => '30.000,07' }] }
+      )
+    end
+    let(:cmf_client) { instance_double(CmfClient, get_uf: cmf_response) }
 
     def perform
       described_class.perform_now(time_report_id)
     end
 
-    before { allow(BsaleClient).to receive(:new).and_return(bsale_client) }
+    before do
+      allow(BsaleClient).to receive(:new).and_return(bsale_client)
+      allow(CmfClient).to receive(:new).and_return(cmf_client)
+    end
+
+    it 'creates cmf client' do
+      perform
+      expect(CmfClient).to have_received(:new)
+    end
+
+    it 'calls get_uf method of cmf client' do
+      perform
+      expect(cmf_client).to have_received(:get_uf).with(Date.current)
+    end
 
     it 'creates bsale client' do
       perform
