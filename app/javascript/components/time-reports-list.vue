591ab35a0cc7ec7f138adfcbf1d2fc2d3a5b50cd
nicolasguridi <nguridi@uc.cl>
2022-09-09 16:18:51 -0400

    refactor(components): use time reports query and mutation in time reports list

--- app/javascript/components/time-reports-list.vue
+++ app/javascript/components/time-reports-list.vue
@@ -1,78 +1,125 @@
 <template>
-  <h1>
-    Reportes del mes
-  </h1>
-  <table>
-    <thead>
-      <tr>
-        <th class="px-2">
-          Proyecto
-        </th>
-        <th class="px-2">
-          Fecha inicio
-        </th>
-        <th class="px-2">
-          Fecha fin
-        </th>
-        <th class="px-2">
-          Horas facturables
-        </th>
-        <th class="px-2">
-          Acciones
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr
-        v-for="timeReport in timeReports"
-        :key="timeReport.id"
-      >
-        <td class="px-2">
-          {{ timeReport.project.name }}
-        </td>
-        <td class="px-2">
-          {{ timeReport.from }}
-        </td>
-        <td class="px-2">
-          {{ timeReport.to }}
-        </td>
-        <td class="px-2">
-          {{ timeReport.billableHours }}
-        </td>
-        <td class="px-2">
-          <button
-            v-if="!timeReport.document"
-            @click="createDocument(timeReport.id)"
-          >
-            Facturar
-          </button>
-          <a
-            v-else
-            :href="timeReport.document.url"
-            target="_blank"
-          >
-            Ver factura
-          </a>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="flex flex-col space-y-10">
+    <h1 class="text-3xl">
+      Reportes
+    </h1>
+    <div class="flex items-center space-x-4">
+      <div>
+        <button
+          class="border py-2 px-3"
+          @click="moveMonth(-1)"
+        >
+          &larr;
+        </button>
+        <button
+          class="border-y border-r py-2 px-3"
+          @click="moveMonth(1)"
+        >
+          &rarr;
+        </button>
+      </div>
+      <div class="text-xl">
+        <span class="font-semibold">Mes: </span>{{ currentMonth }}
+      </div>
+    </div>
+    <div
+      v-if="isMutationLoading"
+      class="w-full rounded bg-gray-300 py-2 px-3 text-center"
+    >
+      Cargando...
+    </div>
+    <div
+      v-else-if="isMutationError"
+      class="w-full rounded bg-gray-300 py-2 px-3 text-center"
+    >
+      Error
+    </div>
+    <button
+      v-else
+      class="rounded bg-gray-300 py-2 px-3 hover:bg-gray-400"
+      @click="mutate()"
+    >
+      Obtener reportes del mes actualizados
+    </button>
+    <div
+      v-if="isQueryLoading"
+      class="w-full text-center"
+    >
+      Cargando...
+    </div>
+    <div
+      v-else-if="isQueryError"
+      class="w-full text-center"
+    >
+      Ha ocurrido un error
+    </div>
+    <div
+      v-else-if="data?.data.timeReports.length === 0"
+      class="text-center"
+    >
+      No hay reportes para este mes
+    </div>
+    <table v-else>
+      <thead class="text-left">
+        <tr>
+          <th class="px-2">
+            Proyecto
+          </th>
+          <th class="px-2">
+            Horas facturables
+          </th>
+          <th class="px-2">
+            Acciones
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <time-reports-list-item
+          v-for="timeReport in data?.data.timeReports"
+          :key="timeReport.id"
+          :time-report="timeReport"
+          :selected-date="selectedDate"
+        />
+      </tbody>
+    </table>
+  </div>
 </template>
 <script setup lang="ts">
-import { useMutation } from 'vue-query';
-import documentsApi from '../api/documents';
+import { computed, ref, type Ref } from 'vue';
+import { useMutation, useQuery } from 'vue-query';
+import { addMonths, endOfMonth, format, startOfMonth } from 'date-fns';
+import { es } from 'date-fns/locale';
+import timeReportsApi from '../api/time-reports';
+import timeReportsListItem from './time-reports-list-item.vue';
 
-defineProps<{
-  timeReports: TimeReport[]
-}>();
+const selectedDate = ref(new Date());
+const currentMonth = computed(() => {
+  const startDate = format(startOfMonth(selectedDate.value), 'dd');
+  const endDate = format(endOfMonth(selectedDate.value), 'dd MMMM yyyy', { locale: es });
 
-function useCreateDocumentMutation() {
-  return useMutation((data: { timeReportId: number }) => documentsApi.create(data));
+  return `${startDate} - ${endDate}`;
+});
+
+function moveMonth(direction: number) {
+  selectedDate.value = addMonths(selectedDate.value, direction);
 }
 
-const { mutate } = useCreateDocumentMutation();
-
-function createDocument(timeReportId: number) {
-  mutate({ timeReportId });
+function useTimeReportsQuery(date: Ref<Date>) {
+  return useQuery(['timeReports', date], () => timeReportsApi.index(date.value.toString()));
 }
+
+const { data, isError: isQueryError, isLoading: isQueryLoading, refetch } = useTimeReportsQuery(selectedDate);
+
+function useTimeReportsMutation(date: Ref<Date>) {
+  return useMutation(
+    () => timeReportsApi.create(date.value.toString()),
+    {
+      onSuccess: () => {
+        refetch.value();
+      },
+    },
+  );
+}
+
+const { isError: isMutationError, isLoading: isMutationLoading, mutate } = useTimeReportsMutation(selectedDate);
 </script>
